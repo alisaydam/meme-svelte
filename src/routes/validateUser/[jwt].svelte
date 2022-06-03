29c8f566c9a8b23a6b7d6b467955ad0b35a652b6
KereBere <alsydm091@gmail.com>
2022-06-03 11:45:15 +0300

    Resolved remainig conflicts

--- src/routes/validateUser/[jwt].svelte
+++ src/routes/validateUser/[jwt].svelte
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-<script context="module">
-  export async function load({ params }) {
-    console.log(params.jwt);
-    const validateUser = await  fetch(`https://geyix.herokuapp.com/user/createUser/${params.jwt}`)
-    const user = await validateUser.json()
-    return {
-      props: {
-        user: user
-      },
-    };
-  }
-</script>
-
-<script>
-    export let user
-  console.log(user);
-</script>
-=======
 <script>
   import { onMount } from "svelte";
   import { page } from "$app/stores";
@@ -59,4 +40,3 @@
     border-radius: 15px;
   }
 </style>
->>>>>>> 5b8286e9d3375ca659bdee90c4fac98549113e68
